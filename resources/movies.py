eee0dfc5cd2f3ffda87e9ab133213046119a6056
Daniel Perkins II <daniel.perkins99@gmail.com>
2020-10-10 04:52:54 -0500

    database imported from csv

--- resources/movies.py
+++ resources/movies.py
@@ -5,10 +5,10 @@ from playhouse.shortcuts import model_to_dict
 movie = Blueprint('movies', 'movie')
 
 @movie.route('/', methods=["GET"])
-def get_all_movies():
+def get_hundred_movies():
     ## find the movies and change each one to a dictionary into a new array
     try:
-        movies = [model_to_dict(movie) for movie in models.Movie.select()]
+        movies = [model_to_dict(movie) for movie in models.Movie.select().limit(100)]
         print(movies)
         return jsonify(data=movies, status={"code": 200, "message": "Success"})
     except models.DoesNotExist:
@@ -21,7 +21,6 @@ def create_movies():
     new_movie = models.Movie.create(**body)
     movie_data = model_to_dict(new_movie)
     return jsonify(data=movie_data, status={'code': 200, 'message': 'Success'})
-
 
 @movie.route('/<id>', methods=["GET"])
 def get_one_movie(id):
